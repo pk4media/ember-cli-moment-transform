7bb094665dbb7d6296d53d4c0776e2785d7e7a43
Clairton Rodrigo Heinzen <clairton.rodrigo@gmail.com>
2015-08-04 11:05:02 -0300

    Use ember-cli-moment-transform as addon

--- index.js
+++ index.js
@@ -2,19 +2,5 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-cli-moment-transform',
-
-  included: function included(app) {
-    this.app = app;
-
-    this._super.included(app);
-
-    app.import(app.bowerDirectory + '/moment/moment.js');
-
-    app.import(app.bowerDirectory + '/ember-cli-moment-shim/moment-shim.js', {
-      exports: {
-        moment: ['default']
-      }
-    });
-  }
+  name: 'ember-cli-moment-transform'
 };
